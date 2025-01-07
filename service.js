b0031eb0d043e0e162f8a465c148eccbc519d31f
Angel Barcia <angelbarcia2710@hotmail.com>
2025-01-07 16:56:28 +0100

    Add files via upload

---
+++ service.js
@@ -0,0 +1,9 @@
+const {connection_to_mongodb} = require ("./repository/mongo-repository-base");
+const {createApi} = require ("./api/hr-api");
+
+connection_to_mongodb().then( response => {
+    console.log(`Connected to the MongoDB instance running at ${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`);
+    createApi(() => {
+        console.info(`API is listening on PORT ${process.env.MONGO_PORT || 8100}`);
+    }) 
+})
