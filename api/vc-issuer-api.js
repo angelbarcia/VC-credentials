1cea742006df289e3e0cbf6e1720ec2b25e07c25
Angel Barcia <angelbarcia2710@hotmail.com>
2025-01-29 14:29:06 +0100

    test updated

--- api/vc-issuer-api.js
+++ api/vc-issuer-api.js
@@ -3,73 +3,73 @@ const api = express();
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 const {
-    instantiateVerifiableCredentials,
-    getVerifiableCredentialsById,
-    verifyVerifiableCredentials,
+  instantiateVerifiableCredentials,
+  getVerifiableCredentialsById,
+  verifyVerifiableCredentials,
 } = require("../repository/mongo-repository-vc");
 
 const createApi = (callbackFun) => {
-    api.use(bodyParser.json({limit: "5mb"}));
-    api.use(logger("dev"));
-    api.use((req, res, next) => {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept"
-        );
-        res.header(
-            "Access-Control-Allow-Methods",
-            "HEAD,OPTIONS,POST,PUT,PATCH,DELETE,GET"
-        );
-        next();
-    });
+  api.use(bodyParser.json({ limit: "5mb" }));
+  api.use(logger("dev"));
+  api.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "HEAD,OPTIONS,POST,PUT,PATCH,DELETE,GET"
+    );
+    next();
+  });
 
-    api.get("/vc-issuer/api/v1/health", (req, res) => {
+  api.get("/vc-issuer/api/v1/health", (req, res) => {
+    res.set("Content-Type", "application/json");
+    res.status(200).send({ status: "OK" });
+  });
+
+  api.get("/vc-issuer/api/v1/credentials/:id", (req, res) => {
+    const { id } = req.params;
+    getVerifiableCredentialsById(id)
+      .then((certificate) => {
         res.set("Content-Type", "application/json");
-        res.status(200).send({"status": "OK"});
-    });
+        res.status(200).send(certificate);
+      })
+      .catch((error) => {
+        res.status(400).send(error);
+      });
+  });
 
-    api.get("/vc-issuer/api/v1/credentials/:id", (req, res) => {
-        const {id} = req.params;
-        getVerifiableCredentialsById(id)
-            .then((certificate) => {
-                res.set("Content-Type", "application/json");
-                res.status(200).send(certificate);
-            })
-            .catch((error) => {
-                res.status(400).send(error);
-            });
-    });
+  api.post("/vc-issuer/api/v1/verify", (req, res) => {
+    const vc = req.body;
+    verifyVerifiableCredentials(vc)
+      .then((result) => {
+        res.set("Content-Type", "application/json");
+        res.status(200).send(result);
+      })
+      .catch((error) => {
+        res.status(400).send({ error });
+      });
+  });
+  // instantiating a VC
+  api.post("/vc-issuer/api/v1/credentials", (req, res) => {
+    const { id, updatedProof } = req.body;
+    instantiateVerifiableCredentials({ id, updatedProof })
+      .then((certificate) => {
+        res.set("Content-Type", "application/json");
+        res.status(201).send(certificate);
+      })
+      .catch((error) => {
+        res.status(400).send({ error });
+      });
+  });
 
-    api.post("/vc-issuer/api/v1/verify", (req, res) => {
-        const vc = req.body;
-        verifyVerifiableCredentials(vc)
-            .then((result) => {
-                res.set("Content-Type", "application/json");
-                res.status(200).send(result);
-            })
-            .catch((error) => {
-                res.status(400).send({error});
-            });
-    });
-    // instantiating a VC
-    api.post("/vc-issuer/api/v1/credentials", (req, res) => {
-        const {id, updatedProof} = req.body;
-        instantiateVerifiableCredentials({id, updatedProof})
-            .then((certificate) => {
-                res.set("Content-Type", "application/json");
-                res.status(201).send(certificate);
-            })
-            .catch((error) => {
-                res.status(400).send({error});
-            });
-    });
-
-    const server = api.listen(process.env.API_PORT || 8100, callbackFun);
+  const server = api.listen(process.env.API_PORT || 8100, callbackFun);
 }; // createApi
 
 exports.getApi = () => {
-    return api;
+  return api;
 };
 
 exports.createApi = createApi;
--- app.test.js
+++ app.test.js
@@ -1,15 +1,64 @@
-const request = require('supertest');
-const {createApi, getApi} = require("./api/vc-issuer-api");
-createApi(()=>{})
+const request = require("supertest");
+const { createApi, getApi } = require("./api/vc-issuer-api");
+createApi(() => {});
 api = getApi();
 
-describe('GET /vc-issuer/api/v1/health', () => {
-    it('should return a status of 200 and a JSON response', async () => {
-        const response = await request(api).get('/vc-issuer/api/v1/health');
+describe("GET /vc-issuer/api/v1/health", () => {
+  it("should return a status of 200 and a JSON response", async () => {
+    const response = await request(api).get("/vc-issuer/api/v1/health");
 
-        // Assertions
-        expect(response.statusCode).toBe(200);
-        expect(response.body).toEqual({ status: 'OK' });
-        expect(response.headers['content-type']).toMatch(/json/);
-    });
+    // Assertions
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ status: "OK" });
+    expect(response.headers["content-type"]).toMatch(/json/);
+  });
+});
+
+describe("GET /vc-issuer/api/v1/credentials/:id", () => {
+  it("should return a status of 200 and a JSON response", async () => {
+    const response = await request(api).get(
+      "/vc-issuer/api/v1/credentials/:id"
+    );
+
+    // Assertions
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ status: "OK" });
+    expect(response.headers["content-type"]).toMatch(/json/);
+  });
+});
+
+describe("POST /vc-issuer/api/v1/verify", () => {
+  it("should return a status of 200 and a JSON response", async () => {
+    const response = await request(api).post("/vc-issuer/api/v1/verify");
+
+    // Assertions
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({ status: "OK" });
+    expect(response.headers["content-type"]).toMatch(/json/);
+  });
+});
+
+describe("POST /vc-issuer/api/v1/credentials", () => {
+  it("should return a status of 201 and a JSON response when valid data is sent", async () => {
+    const response = await request(api)
+      .post("/vc-issuer/api/v1/credentials")
+      .send({
+        id: "valid-id",
+        updatedProof: {
+          type: "Ed25519Signature2020",
+          created: "2025-01-01T12:00:00Z",
+          verificationMethod: "did:example:deepcloudlabs#rsa-public-key-1",
+          proofPurpose: "assertionMethod",
+          jws: "eyJhbGciOiJFZERTQSJ9..MEUCIQCY12345EXAMPLESIGNEDDATA12345rEQHgIerA",
+        },
+      })
+      .set("Accept", "application/json");
+
+    // Assertions
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body).toHaveProperty("updatedProof");
+    expect(response.body).toEqual({ status: "OK" });
+    expect(response.headers["content-type"]).toMatch(/json/);
+  });
 });
